f97a719e74603b21d9c296517dcd39511983d08d
Vaibhav Karav <41640139+vaibhavkarav@users.noreply.github.com>
2021-03-25 21:12:30 +0530

    Add files via upload

---
+++ Pair Sum/pair_sum.py
@@ -0,0 +1,31 @@
+from sys import stdin
+
+
+def pairSum(arr, n, x) :
+    k = 0
+    for i in range(n):
+        for j in range(i + 1, n):
+            if arr[i] + arr[j] == x:
+                k += 1
+    return k
+            
+#Taking Input Using Fast I/O
+def takeInput() :
+    n = int(stdin.readline().strip())
+    if n == 0 :
+        return list(), 0
+
+    arr = list(map(int, stdin.readline().strip().split(" ")))
+    return arr, n
+
+
+#main
+t = int(stdin.readline().strip())
+
+while t > 0 :
+    
+    arr, n = takeInput()
+    x = int(stdin.readline().strip())
+    print(pairSum(arr, n, x))
+
+    t -= 1
